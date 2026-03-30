d063eea037ea4be8668f4c7fb9ca97334c660f0d
JoshSmith84 <jas426@icloud.com>
2026-03-30 10:42:35 -0500

    B: changed to use dictionaries for better re-usability in the future

--- cwa_webroot.py
+++ cwa_webroot.py
@@ -9,7 +9,7 @@ output_list = []
 new_headers = ['Device Name',
                'Domain',
                'Last logged on user',
-               'OS Type',
+               'OS',
                'Current AV',
                'Last seen in CWA',
                'Last seen in WR',
@@ -20,70 +20,84 @@ cwa_device_name_list = []
 
 
 def read_csv(input_csv):
-    out_list = []
-    with open(input_csv, encoding='utf-8') as csv_file:
-        headers = csv_file.readline().strip('\n').split(',')
-        reader = csv.reader(csv_file)
-        for row in reader:
-            out_list.append(row)
-    return headers, out_list
+    with open(input_csv, encoding='utf-8-sig') as csv_file:
+        return list(csv.DictReader(csv_file))
+
+
+def apply_mapping(records: list[dict], mapping: dict) -> list[dict]:
+    """
+    Takes a list of dicts and applies a mapping of fieldnames to new fieldnames.
+
+    :param records: list of dicts
+    :param mapping: dict where keys are new fieldnames and values are old fieldnames.
+    :return: list of dicts
+    """
+    return [
+        {out_key: record[in_key].strip() for out_key, in_key in mapping.items()}
+        for record in records
+    ]
 
 
 if __name__ == '__main__':
 
-    cwa_headers, cwa_list = read_csv(cw_input_file)
-    wr_headers, wr_list = read_csv(wr_input_file)
+    out_dict_list = []
 
+    cwa_list = read_csv(cw_input_file)
+    wr_list = read_csv(wr_input_file)
 
-    for wr_device in wr_list:
-        wr_device_name_list.append(wr_device[0].strip())
+    cwa_map = {
+        "Device Name": "Computer Name",
+        "Domain": "Domain",
+        "Last logged on user": "Last Logged in User",
+        "OS": "OS",
+        "Current AV": "Virus Scanner",
+        "Last seen in CWA": "Last Contact",
+    }
 
-    for cwa_device in cwa_list:
-        cwa_device_name_list.append(cwa_device[1].strip())
+    wr_map = {
+        "Device Name": "Name",
+        "Domain": "Work Group",
+        "Last logged on user": "Current User",
+        "OS": "OS",
+        "Last seen in WR": "Last Seen",
+    }
 
-    for device in cwa_list:
-        prepped_device = []
-        if device[1].strip() in wr_device_name_list:
-            for wr_device in wr_list:
-                if device[1].strip() == wr_device[0].strip():
-                    wr_device_last_seen = wr_device[5].strip()
-        else:
-            wr_device_last_seen = 'not in Webroot'
-        hcc = device[13].strip() # "hcc" = horrible cwa checkin...
-        fixed_cwa_last_checkin = (f"{hcc[5:7]}/{hcc[8:10]}/{hcc[0:4]} {hcc[11:16]}")
+    reformated_cwa_list = apply_mapping(cwa_list, cwa_map)
+    reformated_wr_list = apply_mapping(wr_list, wr_map)
 
-        prepped_device.extend([device[1].strip(),
-                               device[9].strip(),
-                               device[14].strip(),
-                               device[5].strip(),
-                               device[11].strip(),
-                               fixed_cwa_last_checkin,
-                               wr_device_last_seen,
-                               ])
-        output_list.append(prepped_device)
+    for cw_device in reformated_cwa_list:
+        out_dict = {}
+        for mapping in cwa_map:
+            out_dict[mapping] = cw_device[mapping]
+        for wr_device in reformated_wr_list:
+            if out_dict['Device Name'] == wr_device['Device Name']:
+                out_dict['Last seen in WR'] = wr_device['Last seen in WR']
+        if "Last seen in WR" not in out_dict:
+            out_dict['Last seen in WR'] = "Not in Webroot"
 
-    for device in wr_list:
-        prepped_device = []
-        if device[0].strip() in cwa_device_name_list:
+        hcc = out_dict['Last seen in CWA'].strip() # "hcc" = horrible cwa checkin...
+
+        # fix the hcc format using string slicing
+        out_dict['Last seen in CWA'] = f"{hcc[5:7]}/{hcc[8:10]}/{hcc[0:4]} {hcc[11:16]}"
+
+        out_dict_list.append(out_dict)
+
+    for wr_device in reformated_wr_list:
+        match = 0
+        for device in out_dict_list:
+            if wr_device['Device Name'] == device['Device Name']:
+                match += 1
+        if match > 0:
             continue
-        if 'Server' in device[14]:
-            wr_os_type = 'Server'
         else:
-            wr_os_type = 'Workstation'
-        prepped_device.extend([device[0].strip(),
-                               device[18].strip(),
-                               device[19].strip(),
-                               wr_os_type,
-                               'Webroot SecureAnywhere 64bit',
-                               'not in Automate',
-                               device[5].strip(),
-                               ])
-        output_list.append(prepped_device)
+            wr_device['Last seen in CWA'] = "Not in Automate"
+            wr_device['Current AV'] = "Webroot SecureAnywhere 64bit"
+            out_dict_list.append(wr_device)
 
 
+    with open(out_file, 'w', encoding='utf-8', newline='') as csv_file:
+        writer = csv.DictWriter(csv_file, fieldnames=new_headers)
+        writer.writeheader()
+        writer.writerows(out_dict_list)
 
-    with open(out_file, 'w', newline='') as csv_file:
-        writer = csv.writer(csv_file)
-        writer.writerow(new_headers)
-        writer.writerows(output_list)
 
